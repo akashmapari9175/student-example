84953f01aca3f016cc7c2370203e0d71bd037401
akashmapari <akashmapari85@gmail.com>
2024-02-24 16:36:57 +0530

    database added

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +25,7 @@ public class StudentController {
 	public ResponseEntity<Student> saveStudent(@RequestBody Student student)
 	{
 		Student saveStudent = service.saveStudent(student);
-		return new ResponseEntity<Student>(saveStudent,HttpStatus.OK);
+		return new ResponseEntity<Student>(saveStudent,HttpStatus.CREATED);
 	}
 	
 	@GetMapping("/getAllStudent")
@@ -32,5 +34,16 @@ public class StudentController {
 		return new ResponseEntity<List<Student>>(allStudent,HttpStatus.OK);
 	}
 	
+	@GetMapping("/getStudent/{id}")
+	public ResponseEntity<Student> getStudentById(@PathVariable int id){
+		Student studentById = service.getStudentById(id);
+		return new ResponseEntity<Student>(studentById,HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteById(@PathVariable("id") int idt) {
+		service.deleteById(idt);
+		return "Student deleted successfully ";
+	}
 	
 }
