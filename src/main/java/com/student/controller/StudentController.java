bce6affcbf0f39c88d0640923001230e42a4e4ce
akashmapari <akashmapari85@gmail.com>
2024-02-24 16:21:24 +0530

    update

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -1,8 +1,11 @@
 package com.student.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +26,11 @@ public class StudentController {
 		return new ResponseEntity<Student>(saveStudent,HttpStatus.OK);
 	}
 	
+	@GetMapping("/getAllStudent")
+	public ResponseEntity<List<Student>> getAllStudent(){
+		List<Student> allStudent = service.getAllStudent();
+		return new ResponseEntity<List<Student>>(allStudent,HttpStatus.OK);
+	}
 	
 	
 }
